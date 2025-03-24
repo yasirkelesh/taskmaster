422ab44c250b2597797fd1b3715473bceda7361a
mukeles <keles4112@gmail.com>
2025-03-24 23:13:03 +0300

    loger

--- logger/logger.go
+++ logger/logger.go
@@ -9,12 +9,16 @@ var logger *log.Logger
 
 func init() {
 	file, err := os.OpenFile("taskmaster.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 	logger = log.New(file, "TASKMASTER: ", log.LstdFlags)
+
 }
 
 func Log(msg string) {
 	logger.Println(msg)
+	logger.SetPrefix("TASKMASTER: ")
+
 }
